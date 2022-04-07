955f68fee43207a77a9105d13bac30170d65ad66
wangwuzhe <wuzhe-wang@zju.edu.cn>
2022-04-07 09:24:15 +0800

    Revise slv_fsm

--- generators/rtl/test/slv_fsm.v
+++ generators/rtl/test/slv_fsm.v
@@ -7,116 +7,101 @@ module slv_fsm
     )
     (
         clk, rst_n,
-        req_vld_m, req_rdy_m, ack_vld_m, ack_rdy_m,
-        req_vld_s, req_rdy_s, ack_vld_s, ack_rdy_s,
-
-        rd_en, wr_en, addr, wr_data, rd_data,
-        g_srst_up, g_srst_down,
-        rd_data_vld_in,
-        addr_decode,
-        wr_data_ff,
-        wr_en_ff,
-        rd_en_ff
+        // global synchronous reset
+        glb_srst_up, glb_srst_down,
+        // req and ack handshake of reg_native_if between upstream and current regslv module
+        req_vld_up, req_rdy_up, ack_vld_up, ack_rdy_up,
+        // control signals and info of reg_native_if between upstream and current regslv module
+        rd_en_up, wr_en_up, addr_up, wr_data_up, rd_data_up,
+        // req and ack handshake of reg_native_if between current regslv and downstream modules
+        req_vld_down, req_rdy_down, ack_vld_down, ack_rdy_down,
+        // control signals and info of reg_native_if between current regslv and downstream modules
+        rd_en_down, wr_en_down, addr_down, wr_data_down, rd_data_down
     );
 
 localparam EXT_NUM = M ? M : 1;
 localparam REG_NUM = N ? N : 1;
 
+localparam  IDLE = 2'b00, // no operation
+            LOCK = 2'b01, // get req_vld_up from master, lock the input signal
+            WAIT = 2'b10, // if slave is not ready, wait slave req_rdy_down
+            ACK =  2'b11; // wait slave ack_vld_up
 
-// state decode
-localparam   IDLE = 2'd0; // no operation
-localparam   LOCK = 2'd1; // get req_vld_m from master, lock the input signal
-localparam   WAIT = 2'd2; // if slave is not ready, wait slave req_rdy_s
-localparam   ACK =  2'd3; // wait slave ack_vld_m
-//reg_slv_if in
-input clk;
-input rst_n;
-input req_vld_m;
-input rd_en;
-input wr_en;
-input [ADDR_WIDTH-1:0] addr;
-input [DATA_WIDTH-1:0] wr_data;
-// decoder in
-input g_srst_up;
-input [DATA_WIDTH-1:0] rd_data_vld_in;
-input ack_vld_s;
-// internal/external reg control signal
-output reg g_srst_down;
-output [ADDR_WIDTH-1:0] addr_decode;
-output reg [DATA_WIDTH-1:0] wr_data_ff;
-output reg wr_en_ff;
-output reg rd_en_ff;
-// reg_slv_if handshake
-input ack_rdy_m;
-output reg ack_rdy_s;
-output reg req_rdy_m;
-// EXT_NUM external modules and 1 internal modules req_rdy_s = {ext_req_rdy,internal}
-input req_rdy_s;
+input logic clk;
+input logic rst_n;
 
-output [DATA_WIDTH-1:0] rd_data;
-output ack_vld_m;
-output reg req_vld_s;
+input logic glb_srst_up;
+output logic glb_srst_down;
 
-// machine state value
-reg [1:0] next_state;
-reg [1:0] state;
+input logic req_vld_up;
+output logic req_rdy_up;
+output logic ack_vld_up;
+input logic ack_rdy_up;
 
-//
-reg [ADDR_WIDTH-1:0] addr_ff;
+input logic rd_en_up;
+input logic wr_en_up;
+input logic [ADDR_WIDTH-1:0] addr_up;
+output logic [DATA_WIDTH-1:0] rd_data_up;
+input logic [DATA_WIDTH-1:0] wr_data_up;
+
+output logic req_vld_down;
+input logic req_rdy_down;
+input logic ack_vld_down;
+output logic ack_rdy_down;
+
+output logic rd_en_down;
+output logic wr_en_down;
+output logic [ADDR_WIDTH-1:0] addr_down;
+input logic [DATA_WIDTH-1:0] rd_data_down;
+output logic [DATA_WIDTH-1:0] wr_data_down;
+
+logic [1:0] state;
+logic [1:0] next_state;
 
 // state register
-always_ff@(posedge clk or negedge rst_n)begin
-    if(!rst_n)begin
-        state <= 2'b0;
-    end
-    else begin
+always_ff @(posedge clk or negedge rst_n) begin
+    if(~rst_n || glb_srst_up)
+        state <= IDLE;
+    else
         state <= next_state;
-    end
 end
 
-// state transfer logic
+// state transition logic
 always_comb begin
     case(state)
         IDLE:begin
-            next_state = req_vld_m ? LOCK : IDLE;
+            next_state = req_vld_up ? LOCK : IDLE;
         end
         LOCK:begin
-            next_state = ack_vld_m ? IDLE :(req_rdy_s ? ACK : WAIT);
+            next_state = ack_vld_up ? IDLE : (req_rdy_down ? ACK : WAIT);
         end
         WAIT:begin
-            next_state = req_rdy_s ? ACK : WAIT;
+            next_state = req_rdy_down ? ACK : WAIT;
         end
         ACK:begin
-            next_state = (ack_vld_m | g_srst_up) ? IDLE : ACK;
+            next_state = ack_vld_up ? IDLE : ACK;
         end
         default: next_state = IDLE;
     endcase
 end
 
 // output logic
-always_ff@(posedge clk or negedge rst_n)begin
-    if(!rst_n)begin
-        req_vld_s <= 0;
-        req_rdy_m <= 0;
-        addr_ff <= 0;
-        wr_en_ff <= 0;
-        rd_en_ff <= 0;
-        wr_data_ff <= 0;
-    end
-    else begin
-        req_rdy_m <=  (next_state == IDLE) ? 1'b1 : 1'b0;
-        addr_ff <= (next_state == LOCK) ? addr : addr_ff;
-        req_vld_s <= (next_state == LOCK || next_state == WAIT) ? 1'b1 : 1'b0;
-        wr_en_ff <= (next_state == LOCK || next_state == WAIT) ? wr_en : 1'b0;
-        rd_en_ff <= (next_state == LOCK || next_state == WAIT) ? rd_en : 1'b0;
-        wr_data_ff <= (next_state == LOCK) ? wr_data : wr_data_ff;
-    end
+always_ff @(posedge clk or negedge rst_n) begin
+    if(!rst_n)
+        wr_data_down <= 0;
+    else
+        wr_data_down <= (next_state == LOCK) ? wr_data_up : wr_data_down;
 end
 
-assign ack_rdy_s = (state == ACK);
-assign g_srst_down = g_srst_up;
-assign rd_data = ack_vld_s ? rd_data_vld_in : rd_data; // ???
-assign ack_vld_m = ack_vld_s;
-assign addr_decode = (state == IDLE) ? addr : addr_ff;
+assign req_rdy_up = (state == IDLE);
+assign req_vld_down = (state == LOCK || state == WAIT);
+assign wr_en_down = (state == LOCK || state == WAIT) ? wr_en_up : 1'b0;
+assign rd_en_down = (state == LOCK || state == WAIT) ? rd_en_up : 1'b0;
+
+assign ack_rdy_down = (state == ACK);
+assign glb_srst_down = glb_srst_up;
+assign rd_data_up = ack_vld_down ? rd_data_down : rd_data_up; // ???
+assign ack_vld_up = ack_vld_down;
+assign addr_down = addr_up;
 
 endmodule
